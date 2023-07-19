6d276beead49691a4dbf9c7171fe0324991a5426
Vitor <marthendalnunes@gmail.com>
2023-07-19 01:30:07 -0300

    chore: test

--- integrations/moralis/hooks/transaction/use-get-transaction.ts
+++ integrations/moralis/hooks/transaction/use-get-transaction.ts
@@ -11,12 +11,14 @@ export function useGetTransaction({ chain, transactionHash, enabled }: GetTransa
   return useQuery(['get-transaction', chain, transactionHash], {
     queryFn: async () => {
       const res = await fetch(`/integration/moralis/api/transaction/getTransaction?chain=${chain}&transactionHash=${transactionHash}&format=result`)
-      console.log('debug res', res)
-      if (!res.ok) throw new Error(String(res))
+      const testRes = await res.text()
+      console.log('debug res', testRes)
+      if (!res.ok) throw new Error(testRes)
 
       return res.json() as Promise<GetTransactionResponse>
     },
     enabled,
+    useErrorBoundary: true,
   })
 }
 
